9cf20ff77e1b0f6ed1007b8158056c2f1fa426a8
Yan Zhang <31453854+yanzhang01@users.noreply.github.com>
2017-11-02 22:50:57 -0400

    Update app.R

--- Beta_version/app.R
+++ Beta_version/app.R
@@ -45,7 +45,7 @@ server <- function(input, output) {
   # Tab4
   output$readme <- renderUI({  
     fluidRow(
-      column(width = 6, offset = 2,
+      column(width = 6, offset = 1,
         includeMarkdown("README.md")
       )
     )
