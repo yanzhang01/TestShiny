95ee84168b2017546f09a1c51949612bbf553a7f
Yan Zhang <31453854+yanzhang01@users.noreply.github.com>
2017-11-03 00:18:02 -0400

    Update app.R

--- Beta_version/app.R
+++ Beta_version/app.R
@@ -45,7 +45,15 @@ server <- function(input, output) {
         ),
 
         mainPanel(
-        plotOutput("distPlot")
+          #plotOutput("distPlot")
+          # create fake data
+          src <- c("A", "A", "A", "A",
+                   "B", "B", "C", "C", "D")
+          target <- c("B", "C", "D", "J",
+                      "E", "F", "G", "H", "I")
+          networkData <- data.frame(src, target)
+          # plot
+          simpleNetwork(networkData)
         )
       )
     )
