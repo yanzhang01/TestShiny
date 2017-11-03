134d872c806f79a7a0d049c1a52aec711d68a430
Yan Zhang <31453854+yanzhang01@users.noreply.github.com>
2017-11-03 01:04:18 -0400

    Update app.R

--- Beta_version/app.R
+++ Beta_version/app.R
@@ -6,7 +6,7 @@ require(RCurl)
 
 # Define UI for the app
 ui <- navbarPage(
-  title = 'PgenePapers (beta version):',
+  title = 'PgenePapers (beta version) Search Tools:',
   tabPanel('Pseudogene-gene-role table', DT::dataTableOutput('ex1')),
   tabPanel('All papers', DT::dataTableOutput('ex2')),
   tabPanel('Graph presentation', uiOutput('graph_presentation')),
