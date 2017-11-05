4bd0885b8406b5375b40ba8cd55b94f7278e4577
Yan Zhang <31453854+yanzhang01@users.noreply.github.com>
2017-11-05 16:27:45 -0500

    Update app.R

--- Beta_version/app.R
+++ Beta_version/app.R
@@ -52,7 +52,7 @@ server <- function(input, output) {
                     c("All",
                       sort(unique(as.character(dat$"Coding Gene Name")))))
         ),
-        column(4, actionButton("button", "Save network to HTML"))
+        column(4, "Click\n", actionButton("button", "Save network to HTML"))
       ),
     
       # Create a new row for the graph
