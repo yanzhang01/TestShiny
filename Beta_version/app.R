7f7168c59197ce242da78a85100082a573b19a7f
Yan Zhang <31453854+yanzhang01@users.noreply.github.com>
2017-11-05 18:00:47 -0500

    Update app.R

--- Beta_version/app.R
+++ Beta_version/app.R
@@ -68,7 +68,7 @@ server <- function(input, output) {
       # Creat a button to save network data
       fluidRow(
         column(12, "Click the button to", downloadButton("downloadData", "Download filtered data"), 
-               "You can also right click on your mouse and print the graph into a PDF file.")
+               ". You can also right click on your mouse and print the graph into a PDF file.")
       ),
       
       # Create a new row for the graph
